a87ab13f8c7281d590d951c5efaec84045e7fe99
Saiswarupb <saiswarupb@ideyalabs.com>
2023-04-11 01:17:56 +0530

    Updated jobs screens

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,10 +1,16 @@
-import React from 'react'
-import Footer from '../../Components/Footer/Footer'
-import Header from '../../Components/Header/Header'
-import Profile from '../../Components/Profile/Profile'
+import React from "react";
+import Footer from "../../Components/Footer/Footer";
+import Header from "../../Components/Header/Header";
+import Profile from "../../Components/Profile/Profile";
+import iHire from '../../Assets/Images/iHire-bg.jpg';
 
 export default function HomePage() {
   return (
-    <div><Header/><Profile/><Footer/></div>
-  )
+    <div>
+      <Header />
+      <Profile />
+      <Footer />
+    </div>
+  );
 }
+
