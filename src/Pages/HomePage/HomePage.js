1e1da64a1b1fdcd815b33461764c7ab134ec4bb3
NikitaKavde24 <nikitak@ideyalabs.com>
2023-04-10 15:50:47 +0530

    Added profile

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Footer from '../../Components/Footer/Footer'
 import Header from '../../Components/Header/Header'
+import Profile from '../../Components/Profile/Profile'
 
 export default function HomePage() {
   return (
-    <div className='h-full'><Header/><Footer/></div>
+    <div className='h-full'><Header/><Profile/><Footer/></div>
   )
 }
