42928a2a90d4596e763c6f0a4701ba01362d6904
Saiswarupb <saiswarupb@ideyalabs.com>
2023-04-11 23:44:49 +0530

    Added tracker fields

--- src/Components/ModalDialog/ModalDialog.js
+++ src/Components/ModalDialog/ModalDialog.js
@@ -6,6 +6,7 @@ export default function ModalDialog({
   open,
   handleClose,
   children,
+  maxWidth,
   className = "",
 }) {
   // const Transition = React.forwardRef(function Transition(props, ref) {
@@ -14,25 +15,24 @@ export default function ModalDialog({
 
   return (
     <Dialog
-    open={open}
-    // TransitionComponent={Transition}
-    disableEnforceFocus={true}
-    onClose={handleClose}
-    aria-describedby="alert-dialog-slide-description"
-  >
-    <div className={`min-w-[600px] min-h-[300px] p-4 ${className}`}>
-      <div className="text-right ">
-        <CloseRoundedIcon
-          onClick={() => {
-            handleClose();
-          }}
-          className="cursor-pointer"
-        />
+      open={open}
+      maxWidth={maxWidth}
+      // TransitionComponent={Transition}
+      disableEnforceFocus={true}
+      onClose={handleClose}
+      aria-describedby="alert-dialog-slide-description"
+    >
+      <div className={`min-w-[500px] min-h-[300px] p-4 ${className}`}>
+        <div className="text-right ">
+          <CloseRoundedIcon
+            onClick={() => {
+              handleClose();
+            }}
+            className="cursor-pointer"
+          />
+        </div>
+        <div className="flex justify-center"> {children}</div>
       </div>
-
-      {children}
-    </div>
-  </Dialog>
-    
+    </Dialog>
   );
 }
